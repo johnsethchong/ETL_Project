5136585a19ccf09df37ef130156b9ac39958ae8c
Johnseth Chong <johnsethchong@gmail.com>
2019-11-14 18:59:15 -0800

    Update

--- query.sql
+++ query.sql
@@ -1,24 +1,26 @@
 -- Create tables for raw data to be loaded into
 CREATE TABLE mvp (
-MVP_Name INT PRIMARY KEY,
-MVP_status varchar(30),
-Year int
+year INT PRIMARY KEY,
+name VARCHAR(30),
+finals_status VARCHAR(30)
 );
 
 CREATE TABLE players (
-player_id INT,
-name varchar(30)
+player_id VARCHAR(30) PRIMARY KEY,
+name VARCHAR(30) 
 );
 
 CREATE TABLE salaries (
-player_id INT,
-season_end int,
-salary int
+year INT,
+player_id VARCHAR(30),
+salary INT
 );
 
 
--- Joins tables
-SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
-FROM customer_name
-JOIN customer_location
-ON customer_name.id = customer_location.id;
+
+
+-- -- Joins tables
+-- SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
+-- FROM customer_name
+-- JOIN customer_location
+-- ON customer_name.id = customer_location.id;
